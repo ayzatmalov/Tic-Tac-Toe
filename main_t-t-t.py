51771d41940328d44cf03caf4f50d343d74a0f71
ayzatmalov <ayzat.malov@gmail.com>
2023-06-29 00:36:20 +0300

    B5.6.TTT:welcome_block B5.6.TTT:playing_field_display

--- main_t-t-t.py
+++ main_t-t-t.py
@@ -13,5 +13,36 @@ def hello():
     print(' <      пробел, где:       > ')
     print(' <   "x" - номер строки    > ')
     print(' <   "у" - номер столбца   > ')
+    print(' ')
 
 hello()
+
+# БЛОК 2. ОТОБРАЖЕНИЕ КЛЕТОК И ПОЛЯ
+# Вариант 1
+# field = [
+#     [' ', ' ', ' '],
+#     [' ', ' ', ' '],
+#     [' ', ' ', ' ']
+# ]
+# def view():
+#     print('  | 0 | 1 | 2 | ')
+#     print('-----------------')
+#     for i, j in enumerate(field):
+#         row_str = str(i) + ' | ' + ' ' + ' | ' + ' ' + ' | ' + ' ' + ' | ' + ' '.join(map(str, j))
+#         print(row_str)
+#         print('-----------------')
+#     print(' ')
+# view()
+
+# Вариант 2
+field_1 = [[' '] * 3 for i in range(3)] # inline
+#print(field_1)
+def view_1():
+    print('  | 0 | 1 | 2 | ')
+    print('-----------------')
+    for i, j in enumerate(field_1):
+        row_str = f"{i} | {' | '.join(j)} | "
+        print(row_str)
+        print('-----------------')
+    print(' ')
+view_1()
